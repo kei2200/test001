5a76279dd005b11f44241a7978a0dcb6bb4a53a3
Keita Yamamoto <keita.yamamoto001@gmail.com>
2019-08-11 16:58:40 +0900

    コミット

---
+++ jspra/test.js
@@ -0,0 +1,19 @@
+console.log('test');
+
+var x = 30;
+//
+// if (x >= 10){
+//   console.log('変数Xは10以上です');
+// }else{
+//   console.log('変数xは10以下です');
+// }
+
+if (x >= 20){
+  console.log('変数Xは20以上です');
+}else if(x >= 10){
+  console.log('変数Xは10以上です');
+}else{
+  console.log('変数xは10以下です');
+}
+// ネスト
+if (x>=20)
